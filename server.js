a94ec72e497850aa1724a9470532ff63bfc6e3c0
Mary Margaret <mmlawton15@gmail.com>
2022-01-17 16:18:28 -0500

    got node to return the database which appears as an array

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'CrayolaBubble15!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
